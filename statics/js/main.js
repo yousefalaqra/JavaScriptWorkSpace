ce06425661aea024d84338542908c0cdbaf1959f
Yousef Alaqra <56271934+yousefalaqra@users.noreply.github.com>
2020-03-25 09:56:20 +0200

    Cata Generator

--- statics/js/main.js
+++ statics/js/main.js
@@ -12,3 +12,9 @@ function yourAgeInDays(){
 function reset(){
     document.getElementById('result').remove();
 }
+
+function genCat(){
+    var img = document.createElement('img');
+    img.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&size=small';
+    document.getElementById('flex-cat-gen').appendChild(img);
+}
