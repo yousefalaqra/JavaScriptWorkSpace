b85a46f34b6d719fe783fa0df84233b3e5ac99ce
Yousef Alaqra <56271934+yousefalaqra@users.noreply.github.com>
2020-03-25 08:43:03 +0200

    Your Age in Days

---
+++ statics/js/main.js
@@ -0,0 +1,14 @@
+function yourAgeInDays(){
+    var age = prompt("Waht year you were born?");
+    var ageInDays = (2020-age) * 365;
+
+    var h1 = document.createElement('h1');
+    var answer = document.createTextNode('You are ' + ageInDays + ' days old');
+    h1.setAttribute('id', 'result')
+    h1.appendChild(answer);
+    document.getElementById('flex-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('result').remove();
+}
